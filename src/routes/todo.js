6c6218d670a9d19acf576123e52f3a68dd3ee4ec
Gustavo Rodrigues <95060202+gustavorodrii@users.noreply.github.com>
2024-12-30 11:04:57 -0300

    test

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -16,7 +16,7 @@ router.post('/:userId', async (req, res) => {
                 completed: completed || false,
                 userId,
                 reminder: reminder ? new Date(reminder) : null,
-                createdWhen: createdWhen ? new Date(createdWhen) : null,
+                createdWhen,
             },
         });
         res.status(201).json(newTodo);
